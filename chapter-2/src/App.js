b2e6a3e016043490a4f353618c456a2c09d580f1
Sean Gilday <spgilday19@gmail.com>
2025-05-26 16:18:36 -0400

    Routing with react-router

--- chapter-2/src/App.js
+++ chapter-2/src/App.js
@@ -2,12 +2,21 @@ import Header from './components/Header';
 import Profile from './pages/Profile';
 import logo from './assets/logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Projects from './pages/Projects';
+import ProjectDetail from './pages/ProjectDetail';
 
 function App() {
   return (
     <div className="App">
-      <Header logo={logo}/>
-      <Profile userName="SeanGilday"/>
+      <BrowserRouter>
+        <Header logo={logo}/>
+        <Routes>
+          <Route path='/' element={<Profile userName='SeanGilday'/>}/>
+          <Route path='/projects' element={<Projects userName='SeanGilday'/>}/>
+          <Route path='/projects/:name' element={<ProjectDetail userName='SeanGilday'/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
